b12132cceacf32759e10cc1c884beae2de100d75
Anthony Sessa <sessa@fourfiftynine.com>
2012-09-12 23:33:00 -0400

    implemented story list template and structure via underscore.js templating + moment.js time formatting

--- src/js/main.js
+++ src/js/main.js
@@ -6,18 +6,22 @@
  */
 
 /*jshint asi: false, browser: true, curly: true, devel: true, eqeqeq: false, forin: false, newcap: true, noempty: true, strict: true, undef: true */
-/*global jQuery: true */
+/*global jQuery: true, _: true */
 
-(function( window, $, undefined ) {
+(function( window, $, underscore, undefined ) {
 
 'use strict';
 
 var CMG = window.CMG || {};
 
 // convienence vars
-var document  = window.document;
-var history   = window.history;
-var location  = window.location;
+
+var document   = window.document;
+var history    = window.history;
+var location   = window.location;
+var moment     = window.moment; // http://momentjs.com/
+// var Mustache  = window.Mustache;
+// var Underscore  = window.Underscore;
 // var Modernizr = window.Modernizr;
 var $window   = $(window);
 var $document = $(document);
@@ -26,16 +30,32 @@ var $document = $(document);
 /** 
  * Slightly modified module pattern
  * 
- * Intantiating both globally available object and locally 
+ * Encapsulation not used in this instance
+ *
+ * Intantiating both globally and locally 
  * available object which creates an easily 
  * accessible "main" object that bypasses the confusion of utilizing
  * the "this" object in complex event scopes
+ *
+ * I find that this comes in handy when you have much much larger
+ * javascript applications that utilize multiple files with
+ * needs for namespacing
  */
 var main = CMG.main = {
 
   // initialization
   init: function() {
+    $.getJSON('../js/uidevtest-data.js', function(data) {
+      var view = $('#story-list').html();
 
+      $.each( data.objects, function( index, article )  {
+        article.formatted_pub_date = moment(article.pub_date).format('h:mm a dddd, MMM. MM, YYYY');
+        article.formatted_updated = moment(article.updated).format('h:mm a dddd, MMM. MM, YYYY');
+        html = underscore.template(view, article);
+        $('#content').append(html);
+      });
+
+    });
   }
 };
 
@@ -45,4 +65,4 @@ $document.ready( main.init );
 // publicize CMG
 window.CMG = CMG;
 
-})( window, jQuery );
+})( window, jQuery, _ );
